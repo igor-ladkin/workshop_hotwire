12575cbffe6a4e252759373d614fad70332d8711
Igor Ladkin <dgkplan@googlemail.com>
2023-08-15 21:21:09 +0200

    Keep track of listeners count

--- app/channels/live_station_channel.rb
+++ app/channels/live_station_channel.rb
@@ -1,9 +1,17 @@
 class LiveStationChannel < ApplicationChannel
   def subscribed
-    puts "hello"
+    station.increment_listeners_count
+    puts "There are #{station.listeners_count} listeners on #{station.name}"
   end
 
   def unsubscribed
-    puts "goodbye"
+    station.decrement_listeners_count
+    puts "There are #{station.listeners_count} listeners on #{station.name}"
+  end
+
+  private
+
+  def station
+    @station ||= LiveStation.find(params[:station_id])
   end
 end
