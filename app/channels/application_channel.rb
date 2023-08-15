eb493fa0185d193557243c7a704416e6decea000
Igor Ladkin <dgkplan@googlemail.com>
2023-08-15 21:54:11 +0200

    Extend application channel with turbo broadcastable

--- app/channels/application_channel.rb
+++ app/channels/application_channel.rb
@@ -1,2 +1,6 @@
 class ApplicationChannel < ApplicationCable::Channel
+  extend Turbo::Streams::Broadcasts
+  extend Turbo::Streams::StreamName
+  include Turbo::Streams::StreamName::ClassMethods
+  include ActionView::RecordIdentifier
 end
